3da7c09cb03c952e10d1eee26db603875d7a5b52
Balasubramaniyan S <balasm2007@gmail.com>
2025-02-21 16:47:34 +0530

    asdf

--- source/app.py
+++ source/app.py
@@ -22,7 +22,26 @@ def data():
     result=con.fetchall()
     con.connection.commit()
     return render_template('add.html',data=result)
-        
+
+@app.route('/sign',methods=['POST','GET'])
+def sign():
+    return render_template('sign.html')
+
+@app.route('/sign1',methods=['POST','GET'])
+def sign1():
+    if request.method == 'POST':
+        name=request.form['name']
+        age=request.form['age']
+        place=request.form['place']  
+        rollno=request.form['rollno']
+        con=conn.connection.cursor(MySQLdb.cursors.DictCursor)
+        query="INSERT INTO user (name,age,place,rollno) values (%s,%s,%s,%s)"
+        result=con.execute(query,(name,age,place,rollno))
+        con.connection.commit()
+        con.close()
+        return redirect (url_for('data'))
+    return render_template('add.html')
     
+
 if __name__=='__main__':
     app.run(debug=True)
