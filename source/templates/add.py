e32c4dcc649b3fae4259a2b8b8b1d9b9e04ef8af
Anbu925957 <anbuv925@gmail.com>
2025-02-21 21:44:44 +0530

    bca

--- source/templates/add.py
+++ source/templates/add.py
@@ -1,1 +1,15 @@
-print('hello')
+from twilio.rest import Client
+import os
+from dotenv import find_dotenv,load_dotenv
+
+
+dotenv_path=find_dotenv()
+load_dotenv(dotenv_path)
+
+
+sid=os.getenv("SID")
+token=os.getenv("TOKEN")
+
+ct=Client(sid,token)
+
+ct.messages.create(body="helo",from_="+13525081703",to='+91 9976775385')
