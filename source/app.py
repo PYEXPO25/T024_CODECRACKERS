5c0f9274cc484d9b6a36cfa7a7523b2b8cffe156
Balasubramaniyan S <balasm2007@gmail.com>
2025-02-21 11:38:46 +0530

    html

--- source/app.py
+++ source/app.py
@@ -1,12 +1,28 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,url_for,flash
+from flask_mysqldb import MySQL,MySQLdb
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST']= "localhost"
+app.config['MYSQL_DB']= "parking"
+app.config['MYSQL_USER']= "root"
+app.config['MYSQL_PASSWORD']= "Kgkite@123"
+app.secret_key="myapp"
+conn = MySQL(app)
 
 @app.route('/')
 def home():
     return render_template ('home.html')
 
-
+@app.route('/data', methods=['POST','GET'])
+def data():
+    con=conn.connection.cursor(MySQLdb.cursors.DictCursor)
+    query="SELECT * FROM user"
+    con.execute(query)
+    result=con.fetchall()
+    con.connection.commit()
+    return render_template()
+        
+    
 if __name__=='__main__':
     app.run(debug=True)
